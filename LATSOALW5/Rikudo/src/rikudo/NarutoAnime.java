3a30532f5d34496d65c6386ed8c26aaa7b61fa04
rafiadiningrat <rafiadiningrat615@gmail.com>
2021-10-15 10:31:34 +0700

    LatsoalW5

---
+++ LATSOALW5/Rikudo/src/rikudo/NarutoAnime.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rikudo;
+
+/**
+ *
+ * @author DELL
+ */
+public class NarutoAnime {
+	public static void main(String[] args) {
+		Sasuke s = new Sasuke();
+		s.printDojutsu();
+		
+		Itachi i = new Sasuke();
+		i.printKekkeiGenkai();
+	}
+}
---
+++ LATSOALW5/Rikudo/src/rikudo/Rikudo.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rikudo;
+
+/**
+ *
+ * @author DELL
+ */
+public class Rikudo {
+	protected String Dojutsu = "Rinnegan";
+	protected String Senjutsu = "Sage Mode";
+}
